22552a90080d73f00d9e1750916d45cc92a67297
tahmanejrtahmane-bit <tahmanejrtahmane@gmail.com>
2026-09-11 21:49:01 +0000

    Use Compose compatible with API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin { jvmToolchain(17) }
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation(platform("androidx.compose:compose-bom:2026.09.00"))
+    implementation(platform("androidx.compose:compose-bom:2026.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
